1c0a686de4f4e9ab395178419c95cda72b3bebab
Lawal Abubakar Babatunde <utility4all@gmail.com>
2024-05-23 10:24:06 +0100

    chore: modified

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -38,7 +38,7 @@ async function main() {
     process.exit(1); // Exit if the value is not as expected
   }
   const constructor = myCallData.compile("constructor", {
-    initial_counter: "john",
+    initial_counter: 1,
     address:
       "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
     initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
